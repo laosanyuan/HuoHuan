5bcfb3ac897073447577f92c9e2244a258031255
一刀 <yhonglai@163.com>
2022-07-16 19:46:07 +0800

    refactor 重构爬取代码结构

---
+++ src/HuoHuan.Test/Plugins/TiebaTest.cs
@@ -0,0 +1,20 @@
+﻿using HuoHuan.Plugins;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuoHuan.Test.Plugins
+{
+    [TestFixture]
+    internal class TiebaTest
+    {
+        [Test]
+        public void TestConfig()
+        {
+            var config = new TiebaConfig();
+        }
+    }
+}
---
+++ src/HuoHuan.Util/FolderUtil.cs
@@ -0,0 +1,19 @@
+﻿namespace HuoHuan.Util
+{
+    public class FolderUtil
+    {
+        public static string AppData
+        {
+            get
+            {
+                var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var path = Path.Combine(appDataPath, SoftwareInfo.Identity);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return path;
+            }
+        }
+    }
+}
