f95a0cfb0223000e95e37d1043b149780653b41b
一刀 <yhonglai@163.com>
2022-09-18 13:41:14 +0800

    fix 修复单元测试错误

--- src/HuoHuan.Core.Test/UrlProviderTest.cs
+++ src/HuoHuan.Core.Test/UrlProviderTest.cs
@@ -1,5 +1,6 @@
 ﻿using HuoHuan.Core.Install.UrlProviders;
 using NUnit.Framework;
+using System.Configuration;
 
 namespace HuoHuan.Core.Test
 {
@@ -8,6 +9,7 @@ namespace HuoHuan.Core.Test
         [Test]
         public void TestGiteeGetUrl()
         {
+            ConfigurationManager.AppSettings["GiteeDownloadUrl"] = "https://gitee.com/ylaosan/huo-huan/releases";
             var provider = new GiteeUrlProvider();
             string url = provider.GetDownloadUrl("1.0.0").Result;
             Assert.That(url, Is.EqualTo("https://gitee.com/ylaosan/huo-huan/releases/download/1.0.0/Setup.exe"));
@@ -16,6 +18,7 @@ namespace HuoHuan.Core.Test
         [Test]
         public void TestGithubGetUrl()
         {
+            ConfigurationManager.AppSettings["GithubDownloadUrl"] = "https://github.com/laosanyuan/HuoHuan/releases";
             var provider = new GithubUrlProvider();
             string url = provider.GetDownloadUrl("1.0.0").Result;
             Assert.That(url, Is.EqualTo("https://github.com/laosanyuan/HuoHuan/releases/download/1.0.0/Setup.exe"));
